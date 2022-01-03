21dad35b312bf0f4878609599bdafc92d22e4a9c
sambuccid <sambuccid@gmail.com>
2022-01-03 19:39:17 +0000

    adding monthName shortcode
    adding monthName shortcode, to use it in the song of month

--- .eleventy.js
+++ .eleventy.js
@@ -8,6 +8,13 @@ module.exports = function(eleventyConfig) {
     // Keeps the same directory structure.
     eleventyConfig.addPassthroughCopy("css");
     
+    eleventyConfig.addShortcode("monthName", function(monthNumber) {
+        monthNumber = parseInt(monthNumber);
+        //needed just to get a date, year and day not really needed
+        const dateMonth=new Date(2021,  monthNumber-1, 1); //month number -1 because it is 0 based in a Date constructor
+        return dateMonth.toLocaleString('en-GB', { month: 'long' });
+    });
+    
     return {
         pathPrefix: "CapoeiraSongbook"
     };
