c52d5cda118dfb9a57edb70d516dd77fadf9487f
CortinaCapoeira <95879706+CortinaCapoeira@users.noreply.github.com>
2024-12-15 12:51:24 +0000

    Refresh general style of website (#88)
    * New colour palette and rename css colour variables
    
    * Use image for Move to top button
    
    * Use image for Move to index button and refactor to extract common code
    
    * Fix secondary color typo and style list of lines for song on phones
    
    * Remove repeated code for mobile section of songs
    
    ---------
    
    Co-authored-by: Sambuccid <sambuccid@gmail.com>

--- song-template.11tydata.js
+++ song-template.11tydata.js
@@ -1,30 +1,54 @@
-const {tooltipIdentification, emptyTextObject} = require("./tooltip-identification")
+const { tooltipIdentification, emptyTextObject } = require("./tooltip-identification")
+
+function getCurrentSongfromData(data) {
+  const songIdx = data.pagination.items[0]
+  return data.songs[songIdx]
+}
+
+function getLinesWithDetails(lines, details) {
+  detailedLines = []
+  function newLine() { return { br: [], en: [], bold: undefined } }
+
+  for (let songLine of lines) {
+    const enText = emptyTextObject()
+    const singleLine = newLine()
+
+    singleLine.br = tooltipIdentification(songLine.br, details)
+
+    enText.text = songLine.en
+    singleLine.en.push(enText)
+    singleLine.bold = songLine.bold
+
+    detailedLines.push(singleLine)
+  }
+
+  return detailedLines
+}
+
+function separateLanguageSections(lines) {
+  function newSection(title, lines) { return { title, lines } }
+  const brSection = newSection(
+    'Portuguese',
+    lines.map(l => ({ text: l.br, bold: l.bold }))
+  )
+  const enSection = newSection(
+    'English',
+    lines.map(l => ({ text: l.en, bold: l.bold }))
+  )
+  return [brSection, enSection]
+}
 
 module.exports = {
   eleventyComputed: {
     linesWithDetails: data => {
-      const songIdx = data.pagination.items[0]
-      const song = data.songs[songIdx]
-      //console.log(song.lines[0].br)
-
-      detailedLines = []
-      function newLine() { return { br: [], en: [] , bold: undefined} }
-
-      for(let songLine of song.lines){
-        const enText = emptyTextObject()
-        const singleLine = newLine()
-
-        singleLine.br = tooltipIdentification(songLine.br, song.details)
-
-        enText.text = songLine.en
-        singleLine.en.push(enText)
-        singleLine.bold = songLine.bold
-
-        detailedLines.push(singleLine)
-      }
-
-      return detailedLines
+      const song = getCurrentSongfromData(data)
+      return getLinesWithDetails(song.lines, song.details)
+    },
+    formattedMobileSections: data => {
+      const song = getCurrentSongfromData(data)
+      const linesWithDetails = getLinesWithDetails(song.lines, song.details)
+      const sections = separateLanguageSections(linesWithDetails)
+      return sections
     }
-    
   }
 };
